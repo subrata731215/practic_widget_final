607904c044e9487455ebf09e2366a0599bfd5d12
Subrata <subratavb2012@gmail.com>
2023-08-09 08:01:34 +0530

    homePage listview.builder reverse

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -18,47 +18,51 @@ class HomePage extends ReactiveStateWidget<HomepageController> {
         title: const Text('Practice'),
       ),
       body: ListView.builder(
-        itemCount: controller.widgetList.length,
-        itemBuilder: (context, index) => Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, controller.widgetList[index].routes);
-            },
-            child: Container(
-              height: 50,
-              decoration: const BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              child: ListTile(
-                leading: Container(
-                  height: 40,
-                  width: 40,
+          //reverse: true,
+          itemCount: controller.widgetList.length,
+          itemBuilder: (context, index) {
+            int reverseIndex = controller.widgetList.length - 1 - index;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(
+                      context, controller.widgetList[reverseIndex].routes);
+                },
+                child: Container(
+                  height: 50,
                   decoration: const BoxDecoration(
-                      shape: BoxShape.circle, color: Colors.white),
-                  child: Center(
-                    child: Text(
-                      (index + 1).toString(),
-                      textAlign: TextAlign.center,
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  child: ListTile(
+                    leading: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle, color: Colors.white),
+                      child: Center(
+                        child: Text(
+                          (reverseIndex + 1).toString(),
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      controller.widgetList[reverseIndex].title,
                       style: const TextStyle(
+                        fontSize: 30,
                         fontWeight: FontWeight.w700,
-                        fontSize: 25,
                       ),
                     ),
                   ),
                 ),
-                title: Text(
-                  controller.widgetList[index].title,
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
               ),
-            ),
-          ),
-        ),
-      ),
+            );
+          }),
     );
   }
 }
