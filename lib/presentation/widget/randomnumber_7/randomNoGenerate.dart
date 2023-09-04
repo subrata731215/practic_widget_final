3b91907b0cbf5438ada1c800de257b2397a97763
Subrata <subratavb2012@gmail.com>
2023-09-04 22:54:15 +0530

    extra

--- lib/presentation/widget/randomnumber_7/randomNoGenerate.dart
+++ lib/presentation/widget/randomnumber_7/randomNoGenerate.dart
@@ -37,6 +37,7 @@ class _RandomNoGenerateState extends State<RandomNoGenerate> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: appbarWidget(context,
               appBarTitle: 'Random No Generate',
