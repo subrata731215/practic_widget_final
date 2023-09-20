f6a83ac300192c62186344f70d0b7d2fe0515078
Subrata <subratavb2012@gmail.com>
2023-09-20 12:57:51 +0530

    SearchBoxAnimation

--- lib/presentation/widget/searchboxanimation/searchboxAnimation.dart
+++ lib/presentation/widget/searchboxanimation/searchboxAnimation.dart
@@ -12,72 +12,148 @@ class SearchBoxAnimationLearn extends StatefulWidget {
 
 class _SearchBoxAnimationLearnState extends State<SearchBoxAnimationLearn> {
   bool isTap = false;
+  bool positionTap = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xff0c0c0c),
-      appBar: appbarWidget(context,
-          appBarTitle: 'SearchBoxAnimation',
-          trailingRoutes: Routes.sharePreference),
-      body: AnimatedCrossFade(
-        duration: const Duration(seconds: 2),
-        alignment: Alignment.center,
-        firstCurve: Curves.easeInToLinear,
-        secondCurve: Curves.easeInToLinear,
-        sizeCurve: Curves.easeIn,
-        crossFadeState:
-            isTap ? CrossFadeState.showSecond : CrossFadeState.showFirst,
-        secondChild: TextField(
-          style: const TextStyle(
-              color: Colors.grey, fontWeight: FontWeight.w700, fontSize: 20),
-          decoration: InputDecoration(
-              hintText: 'Search',
-              hintStyle: const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  letterSpacing: 1,
-                  fontWeight: FontWeight.w700),
-              prefixIcon: IconButton(
+        //  backgroundColor: const Color(0xff0c0c0c),
+        appBar: appbarWidget(context,
+            appBarTitle: 'SearchBoxAnimation',
+            trailingRoutes: Routes.sharePreference),
+        body: Column(
+          children: [
+            Expanded(
+              child: Stack(
+                children: [
+                  Positioned(
+                      left: 20,
+                      top: 30,
+                      child: Container(
+                        height: 200,
+                        width: 300,
+                        color: Colors.teal,
+                        child: Stack(
+                          children: [
+                            AnimatedAlign(
+                              duration: Duration(seconds: 2),
+                              alignment: positionTap
+                                  ? Alignment.topLeft
+                                  : Alignment.bottomRight,
+                              child: const Padding(
+                                padding: EdgeInsets.all(18.0),
+                                child: Text(
+                                  'Subrata',
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )),
+                  AnimatedPositioned(
+                    curve: Curves.easeInToLinear,
+                    duration: const Duration(milliseconds: 1000),
+                    left: positionTap ? 10 : 240,
+                    top: 30,
+                    child: const Icon(
+                      Icons.calendar_month,
+                      size: 90,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            OutlinedButton(
                 onPressed: () {
                   setState(() {
-                    isTap = !isTap;
+                    positionTap = !positionTap;
                   });
                 },
-                icon: const Icon(Icons.arrow_back, color: Colors.grey),
+                child: Text('Tap')),
+            Expanded(
+              flex: 2,
+              child: Container(
+                margin: EdgeInsets.all(20),
+                color: Colors.greenAccent,
+                child: AnimatedAlign(
+
+                  duration: Duration(seconds: 2),
+                  alignment:
+                      positionTap ? Alignment.topRight : Alignment.bottomLeft,
+                  child: AnimatedRotation(
+                    duration: const Duration(seconds: 2),
+
+                    turns: positionTap?9:5,
+                    child: const Icon(
+                      Icons.baby_changing_station,
+                      size: 50,
+                    ),
+                  ),
+                ),
               ),
-              enabledBorder:
-                  const OutlineInputBorder(borderSide: BorderSide.none)),
-        ),
-        firstChild: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text('LazyCount',
-                  style: TextStyle(
-                      fontSize: 22,
-                      color: Color(0xffececec),
-                      letterSpacing: 1,
-                      fontWeight: FontWeight.w700)),
-              Row(
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        setState(() {
-                          isTap = !isTap;
-                        });
-                      },
-                      icon: const Icon(Icons.search_outlined,
-                          size: 30, color: Colors.white)),
-                  const SizedBox(width: 24),
-                  const Icon(Icons.filter_alt, size: 30, color: Colors.white),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          ],
+        ));
   }
 }
+
+// Container(
+// padding: EdgeInsets.all(10),
+// margin: EdgeInsets.all(10),
+// color: Colors.transparent,
+// child: Row(
+// mainAxisAlignment: MainAxisAlignment.spaceBetween,
+// children: [
+// positionTap
+// ? const Text(
+// 'LazyCount',
+// style: TextStyle(
+// color: Colors.white,
+// fontSize: 22,
+// fontWeight: FontWeight.w700),
+// )
+//     : Row(
+// children: [
+// IconButton(
+// onPressed: () {
+// setState(() {
+// positionTap = !positionTap;
+// });
+// },
+// icon: const Icon(
+// Icons.arrow_back,
+// color: Colors.white,
+// )),
+// const SizedBox(width: 20),
+// const Text(
+// 'Search',
+// style: TextStyle(
+// color: Colors.grey,
+// fontSize: 20,
+// fontWeight: FontWeight.w700),
+// )
+// ],
+// ),
+// if (positionTap)
+// Row(
+// children: [
+// InkWell(
+// onTap: () {
+// setState(() {
+// positionTap = !positionTap;
+// });
+// },
+// child: Icon(Icons.search_outlined,
+// color: Colors.white, size: 30),
+// ),
+// SizedBox(width: 24),
+// Icon(Icons.filter_alt, color: Colors.white, size: 30)
+// ],
+// ),
+// ],
+// ),
+// ),
